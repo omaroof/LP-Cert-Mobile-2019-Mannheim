eedd8a3fad5de3a69e6825c7dbd3c4b85bb8ae27
omaroof <omaroof@liveperson.com>
2019-09-27 10:26:19 +0200

    first commit for Phase_0

--- app/src/main/java/com/liveperson/mobilemessagingexercise/MobileMessagingExerciseApplication.java
+++ app/src/main/java/com/liveperson/mobilemessagingexercise/MobileMessagingExerciseApplication.java
@@ -7,20 +7,14 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.liveperson.api.LivePersonIntents;
-import com.liveperson.mobilemessagingexercise.model.ApplicationStorage;
-import com.liveperson.mobilemessagingexercise.receivers.LivePersonBroadcastReceiver;
-import com.liveperson.mobilemessagingexercise.services.LpFirebaseMessagingService;
+
 
 /******************************************************************
  * The main application class for the Mobile Messaging Exercise
  *****************************************************************/
 public class MobileMessagingExerciseApplication extends Application {
 
-    private static final String TAG = MobileMessagingExerciseApplication.class.getSimpleName();
 
-    private static boolean showToastOnCallback = true;
-    private ApplicationStorage applicationStorage;
-    private LivePersonBroadcastReceiver livePersonBroadcastReceiver;
 
     /**
      * Android callback invoked as the application is created
@@ -28,7 +22,6 @@ public class MobileMessagingExerciseApplication extends Application {
     @Override
     public void onCreate () {
         super.onCreate();
-        applicationStorage = ApplicationStorage.getInstance();
 
         //Register the app to receive events from LivePerson
         registerForLivePersonEvents();
@@ -38,9 +31,7 @@ public class MobileMessagingExerciseApplication extends Application {
      * Register to receive events from LivePerson within this application
      */
     private void registerForLivePersonEvents() {
-        livePersonBroadcastReceiver = new LivePersonBroadcastReceiver(this);
-        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(getApplicationContext());
-        localBroadcastManager.registerReceiver(livePersonBroadcastReceiver, LivePersonIntents.getIntentFilterForAllEvents());
+
     }
 
     /**
@@ -50,14 +41,7 @@ public class MobileMessagingExerciseApplication extends Application {
      * If showToastOnCallback is false, the message is logged instead
      */
     public void showToast(String message) {
-        if (showToastOnCallback){
-            //Show the message as a popup
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-        }
-        else {
-            //Log the message
-            Log.d(TAG + "_CALLBACK", message);
-        }
+
     }
 
 }
--- app/src/main/java/com/liveperson/mobilemessagingexercise/model/ApplicationConstants.java
+++ app/src/main/java/com/liveperson/mobilemessagingexercise/model/ApplicationConstants.java
@@ -1,20 +1,20 @@
-package com.liveperson.mobilemessagingexercise.model;
-
-/**
- * Store constants for the entire app, including any services
- */
-public class ApplicationConstants {
-    public static final String BRAND_SERVER_URL = "http://10.0.2.2:3000";
-    //"http://livepersondummyidpmobilecert-env-4.g2iy94pv45.eu-central-1.elasticbeanstalk.com";
-    //"https://local.dev.lprnd.net/dummyidp/authenticate";
-    public static final String LIVE_PERSON_APP_ID = "com.liveperson.mobilemessagingexercise";
-    public static final String FCM_TOKEN_NAME = "fcmToken";
-
-    //TODO - Replace with your app's LiveEngage account number
-    public static final String LIVE_PERSON_ACCOUNT_NUMBER = "30983864";
-
-    public static final int LP_PUSH_NOTIFICATION_ID = 42;
-    public static final String LP_PUSH_NOTIFICATION_CHANNNEL_ID = "lp_push_notification_channel";
-    public static final String LP_IS_FROM_PUSH = "lp_is_from_push";
-
-}
+//package com.liveperson.mobilemessagingexercise.model;
+//
+///**
+// * Store constants for the entire app, including any services
+// */
+//public class ApplicationConstants {
+//    public static final String BRAND_SERVER_URL = "http://10.0.2.2:3000";
+//    //"http://livepersondummyidpmobilecert-env-4.g2iy94pv45.eu-central-1.elasticbeanstalk.com";
+//    //"https://local.dev.lprnd.net/dummyidp/authenticate";
+//    public static final String LIVE_PERSON_APP_ID = "com.liveperson.mobilemessagingexercise";
+//    public static final String FCM_TOKEN_NAME = "fcmToken";
+//
+//    //TODO - Replace with your app's LiveEngage account number
+//    public static final String LIVE_PERSON_ACCOUNT_NUMBER = "30983864";
+//
+//    public static final int LP_PUSH_NOTIFICATION_ID = 42;
+//    public static final String LP_PUSH_NOTIFICATION_CHANNNEL_ID = "lp_push_notification_channel";
+//    public static final String LP_IS_FROM_PUSH = "lp_is_from_push";
+//
+//}
