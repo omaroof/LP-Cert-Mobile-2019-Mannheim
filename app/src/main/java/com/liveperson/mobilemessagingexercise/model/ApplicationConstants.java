bbbea0fef0bb735f1bb5fb2d2e749cf0e9c00c49
omaroof <omaroof@liveperson.com>
2019-10-02 11:30:31 +0200

    updated idp url

--- app/src/main/java/com/liveperson/mobilemessagingexercise/model/ApplicationConstants.java
+++ app/src/main/java/com/liveperson/mobilemessagingexercise/model/ApplicationConstants.java
@@ -4,7 +4,7 @@ package com.liveperson.mobilemessagingexercise.model;
  * Store constants for the entire app, including any services
  */
 public class ApplicationConstants {
-    public static final String BRAND_SERVER_URL = "https://gdr3xb81x4.execute-api.eu-west-1.amazonaws.com/prod";
+    public static final String BRAND_SERVER_URL = "http://livepersondummyidpmobilecert-env-4.g2iy94pv45.eu-central-1.elasticbeanstalk.com";
     public static final String LIVE_PERSON_APP_ID = "com.liveperson.mobilemessagingexercise";
     public static final String FCM_TOKEN_NAME = "fcmToken";
 
