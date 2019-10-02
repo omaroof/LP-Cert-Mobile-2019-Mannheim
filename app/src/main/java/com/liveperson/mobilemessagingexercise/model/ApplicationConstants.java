6790d36531a193a3d140351e9f80918e5f25b56a
omaroof <omaroof@liveperson.com>
2019-10-02 11:20:27 +0200

    updated idp url and added files for http traffic

--- app/src/main/java/com/liveperson/mobilemessagingexercise/model/ApplicationConstants.java
+++ app/src/main/java/com/liveperson/mobilemessagingexercise/model/ApplicationConstants.java
@@ -4,9 +4,8 @@ package com.liveperson.mobilemessagingexercise.model;
  * Store constants for the entire app, including any services
  */
 public class ApplicationConstants {
-    public static final String BRAND_SERVER_URL = "http://10.0.2.2:3000";
-    //"http://livepersondummyidpmobilecert-env-4.g2iy94pv45.eu-central-1.elasticbeanstalk.com";
-    //"https://local.dev.lprnd.net/dummyidp/authenticate";
+    public static final String BRAND_SERVER_URL = "http://livepersondummyidpmobilecert-env-4.g2iy94pv45.eu-central-1.elasticbeanstalk.com";
+    //"http://10.0.2.2:3000";
     public static final String LIVE_PERSON_APP_ID = "com.liveperson.mobilemessagingexercise";
     public static final String FCM_TOKEN_NAME = "fcmToken";
 
